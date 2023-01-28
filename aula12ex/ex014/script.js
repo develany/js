a9cff78793c39be27a055f446fe8201c95fd5a6a
Elany Peixoto <elanysantos5@outlook.com>
2023-01-27 23:11:56 -0300

    exercio 14

---
+++ aula12ex/ex014/script.js
@@ -0,0 +1,23 @@
+function carregar(){
+var msg = document.getElementById('msg')
+var img = document.getElementById('imagem')
+var data = new Date()
+var hora = data.getHours()
+//var hora=9
+msg.innerHTML=`Agora são ${hora} horas.`
+if (hora >= 0 && hora < 12) {
+    //Bom dia
+    img.src = 'fotomanha.png'
+    document.body.style.background='#8780CD'
+} else if (hora >= 12 && hora < 18){
+    //Boa tarde
+    img.src = 'fototarde.png'
+    document.body.style.background='#B55D1E'
+
+} else{
+    //Boa noite
+    img.src = 'fotonoite.png'
+    document.body.style.background='#59446D'
+
+}
+}
