7af50f69b355c9e256f7be4c19f033a9247bc470
Elany Peixoto <elanysantos5@outlook.com>
2023-02-23 21:52:22 -0300

    exe16

---
+++ aula15/ambiente.js
@@ -0,0 +1,9 @@
+let num =[5, 8, 2, 9, 3]
+
+num.sort()
+num.push(1)
+console.log(num)
+console.log(`o vetor tem ${num.length} posições`)
+console.log(num[0])
+let pos = num.indexOf(4)
+console.log(`O valor 8 está na posição ${pos}`)
---
+++ aula16/funcao02.js
@@ -0,0 +1,4 @@
+function soma(n1=0, n2=0){// defindo n=0 serve pra quando n receber valor, ele vai atribuir 0
+    return n1 + n2
+}
+console.log(soma(2,5))
